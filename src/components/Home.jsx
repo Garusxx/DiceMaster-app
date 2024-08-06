ed745138c41202f393ab031608adf2fc98300f8a
Rafal Zakrzewski <86196784+Garusxx@users.noreply.github.com>
2024-08-06 16:41:14 +0100

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,70 @@
 import React, { useRef, useEffect, useState } from "react";
+import * as tf from "@tensorflow/tfjs";
 
 const Home = () => {
   const videoRef = useRef(null);
   const [isCameraOn, setIsCameraOn] = useState(false);
+  const [trainingDataInput, setTrainingDataInput] = useState([]);
+  const [trainingDataOutputs, setTrainingDataOutputs] = useState([]);
+  const [model, setModel] = useState(tf.sequential());
+  const [netFeatureModel, setNetFeatureModel] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [classNames, setClassNames] = useState([]);
+
+
+  // model.add(
+  //   tf.layers.dense({ inputShape: [1024], units: 128, activation: "relu" })
+  // );
+  // model.add(
+  //   tf.layers.dense({ units: classNames.length, activation: "softmax" })
+  // );
+
+  // model.summary();
+
+  // model.compile({
+  //   optimizer: "adam",
+  //   loss: (classNames.length === 2) ? "binaryCrossentropy" : "categoricalCrossentropy",
+  //   metrics: ["accuracy"],
+  // })
+
+ 
+
+  function getherDataForClass(event) {
+    let classNumber = parseInt(event.target.getAttribute("data-class"));
+    console.log("Ghetering :" + classNumber);
+  }
 
   function toggleCamera() {
     setIsCameraOn((prevState) => !prevState);
   }
+
+  async function loadMobileNetFeatureModel() {
+    try {
+      const loadedModel = await tf.loadGraphModel(
+        "https://www.kaggle.com/models/google/inception-v3/TfJs/classification/2",
+        { fromTFHub: true }
+      );
+
+      setNetFeatureModel(loadedModel);
+
+      tf.tidy(() => {
+        if (loadedModel) {
+          let answer = loadedModel.predict(tf.zeros([1, 224, 224, 3]));
+          console.log(answer);
+        } else {
+          console.error("Model is null");
+        }
+      });
+    } catch (error) {
+      console.error("Error loading model:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    loadMobileNetFeatureModel();
+  }, []);
 
   useEffect(() => {
     const constraints = { video: true };
@@ -32,13 +90,16 @@ const Home = () => {
   return (
     <div>
       <h1>DiceMaster 2000</h1>
+      {loading && <p>Loading model...</p>}
+      {!loading && netFeatureModel && <p>Model załadowany poprawnie</p>}
+      {!loading && !netFeatureModel && <p>Nie udało się załadować modelu</p>}
       <div>
         <button onClick={toggleCamera}>Camera On/Off</button>
       </div>
       <video ref={videoRef} autoPlay />
       <div>
-        <button>Dice One</button>
-        <button>Dice Two</button>
+        <button data-class="1" onClick={getherDataForClass}>Dice One</button>
+        <button data-class="2" onClick={getherDataForClass}>Dice Two</button>
         <div>
           <button>Trein</button>
           <button>Reset</button>
