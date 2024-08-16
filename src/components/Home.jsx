56dbf9c0f1f09f7d16974a310f27b4af1c536f9d
Rafal Zakrzewski <86196784+Garusxx@users.noreply.github.com>
2024-08-16 13:09:34 +0100

    adding training model functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,47 +6,120 @@ const Home = () => {
   const [isCameraOn, setIsCameraOn] = useState(false);
   const [trainingDataInput, setTrainingDataInput] = useState([]);
   const [trainingDataOutputs, setTrainingDataOutputs] = useState([]);
-  const [model, setModel] = useState(tf.sequential());
   const [netFeatureModel, setNetFeatureModel] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [classNames, setClassNames] = useState([]);
-  const [gatheringClass, setGatheringClass] = useState(null);
+  const [isGatheringData, setIsGatheringData] = useState(false);
+  const [model, setModel] = useState(tf.sequential());
+  const [predict, setPredict] = useState(true);
+
+  model.add(
+    tf.layers.dense({ units: 128, inputShape: [1001], activation: "relu" })
+  );
+  model.add(tf.layers.dense({ units: 2, activation: "softmax" }));
+
+  model.compile({
+    optimizer: tf.train.adam(),
+    loss: "binaryCrossentropy",
+    metrics: ["accuracy"],
+  });
+
+  function logProggress(epoch, logs) {
+    console.log(
+      "Epoch: " + epoch + " Loss: " + logs.loss + " Accuracy: " + logs.acc
+    );
+  }
+
+  function reset() {
+    setTrainingDataInput([]);
+    setTrainingDataOutputs([]);
+  }
+
+
+  function predictLoop() {
+    if (predict) {
+      tf.tidy(() => {
+        let videoFrameAsTensor = tf.browser.fromPixels(
+          videoRef.current.div(255)
+        );
+        let resizedFrame = tf.image.resizeBilinear(
+          videoFrameAsTensor,
+          [224, 224],
+          true
+        );
+
+        let imageFeatures = netFeatureModel.predict(resizedFrame.expandDims());
+        let prediction = model.predict(imageFeatures);
+        let predictingArray = prediction.dataSync();
+
+        console.log(
+          "Prediction:",
+          +predictingArray[0].toFixed(2) + " " + predictingArray[1].toFixed(2)
+        );
+      });
+    }
+
+    window.requestAnimationFrame(predictLoop);
+  }
+
+  async function trainModel() {
+    setPredict(false);
+    tf.util.shuffle(trainingDataInput, trainingDataOutputs);
+    let outputAsTensor = tf.tensor1d(trainingDataOutputs, "int32");
+    console.log("Output as tensor:", outputAsTensor);
+    let oenHotOutput = tf.oneHot(outputAsTensor, 2);
+    console.log("One hot output:", oenHotOutput);
+    let inputTensor = tf.stack(trainingDataInput);
+    console.log("Input tensor:", inputTensor);
+    let results = await model.fit(inputTensor, oenHotOutput, {
+      shuffle: true,
+      epochs: 10,
+      batchSize: 5,
+      callbacks: { onEpochEnd: logProggress },
+    });
+
+    outputAsTensor.dispose();
+    oenHotOutput.dispose();
+    inputTensor.dispose();
+    setPredict(true);
+    predictLoop();
+  }
 
   function gatherDataForClass(event) {
-    let classNumber = parseInt(event.target.getAttribute("data-class"));
-    setGatheringClass(classNumber);
-    console.log("Gathering data for class:", classNumber);
-    if (!loading && model && isCameraOn) {
-      gatherData(classNumber);
+    const dataClass = parseInt(event.target.getAttribute("data-class"));
+    console.log("Set class for gathering data:", dataClass);
+
+    if (!loading && netFeatureModel && isCameraOn) {
+      gatherData(dataClass);
     }
   }
 
   function gatherData(dataClass) {
     let counter = 0;
-
+    setIsGatheringData(true);
     const intervalId = setInterval(() => {
-      console.log(`Gathering data: ${counter}`);
+      console.log(`Gathering data for class ${dataClass}: ${counter}`);
 
-      let imageFeatures = tf.tidy(() => {
-        let videoFrame = tf.browser.fromPixels(videoRef.current);
-        let resizedFrame = tf.image.resizeBilinear(
+      tf.tidy(() => {
+        const videoFrame = tf.browser.fromPixels(videoRef.current);
+        const resizedFrame = tf.image.resizeBilinear(
           videoFrame,
           [224, 224],
           true
         );
-        let normalizedFrame = resizedFrame.div(255.0);
-        return netFeatureModel.predict(normalizedFrame.expandDims());
-      });
+        const normalizedFrame = resizedFrame.div(255.0);
+        const imageFeatures = netFeatureModel.predict(
+          normalizedFrame.expandDims()
+        );
 
-      setTrainingDataInput((prev) => [...prev, imageFeatures]);
-      setTrainingDataOutputs((prev) => [...prev, dataClass]);
+        setTrainingDataInput((prev) => [...prev, imageFeatures]);
+        setTrainingDataOutputs((prev) => [...prev, dataClass]);
+      });
 
       counter++;
       if (counter === 10) {
         clearInterval(intervalId);
-        console.log("Zakończono zbieranie danych.");
-        console.log("Training data input:", trainingDataInput);
-        console.log("Training data outputs:", trainingDataOutputs);
+        console.log("Finished gathering data.");
+        setIsGatheringData(false);
       }
     }, 1000);
   }
@@ -84,6 +157,11 @@ const Home = () => {
   }, []);
 
   useEffect(() => {
+    console.log("Training data input:", trainingDataInput);
+    console.log("Training data outputs:", trainingDataOutputs);
+  }, [trainingDataInput, trainingDataOutputs]);
+
+  useEffect(() => {
     const constraints = { video: true };
 
     if (isCameraOn) {
@@ -119,6 +197,7 @@ const Home = () => {
           className="data-collector"
           data-class="1"
           onClick={gatherDataForClass}
+          disabled={isGatheringData || loading}
         >
           Dice One
         </button>
@@ -126,12 +205,13 @@ const Home = () => {
           className="data-collector"
           data-class="2"
           onClick={gatherDataForClass}
+          disabled={isGatheringData || loading}
         >
           Dice Two
         </button>
         <div>
-          <button>Train</button>
-          <button>Reset</button>
+          <button onClick={trainModel}>Train</button>
+          <button onClick={reset}>Reset</button>
         </div>
       </div>
     </div>
